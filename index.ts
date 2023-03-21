97331097eb5941a721dcf6f6e896f8c0c4b07fa4
SamuelKasper <64090949+SamuelKasper@users.noreply.github.com>
2023-03-21 19:08:09 +0100

    mongodb connection + typescript

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import {MongoClient} from "mongodb";
+
 const app = express();
 const port = 8000;
 
@@ -8,6 +10,28 @@ const port = 8000;
 
 app.use(express.static("public"));
 
-app.listen(port, () =>{
+/*app.listen(port, () =>{
     console.log(`listening on ${port}`);
-});
+});*/
+
+async function connectDB(){
+    const uri = "mongodb+srv://samuelnoahkasper:T8Ugwdh9ZhFEe77v@mycluster.fnu9yyz.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+    try{
+        await client.connect();
+        await listDB(client);
+    }catch(e){
+        console.error(e);
+    } finally {
+        await client.close();
+    }    
+}
+
+async function listDB(client: MongoClient){
+    const dbList = await client.db().admin().listDatabases();
+    dbList.databases.forEach(db => {
+        console.log(db.name);
+    })
+} 
+
+connectDB();
