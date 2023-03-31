bf09be5be56ac66b5efae4204e93bb480b5cbd57
SamuelKasper <SamuelNKasper@web.de>
2023-03-31 09:09:23 +0200

    test 5: only jsnObj

--- build/server.js
+++ build/server.js
@@ -29,8 +29,7 @@ app.post('/uplink', (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     //TODO: decide if device is already in db or is a new device
     // use dev_eui and add zeros at the end to fit 12byte and use it as id for mongodb
     let jsonObj = JSON.parse(JSON.stringify(req.body));
-    console.log(jsonObj.data);
-    console.log("end_device_ids: ", jsonObj.data.end_device_ids);
+    console.log(jsonObj);
     /*let dev_eui = jsonObj.data.end_device_ids.dev_eui;
 
     let data = {
