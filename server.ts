56dbefcb9e40204b7e3c1c2487484756694c87b8
SamuelKasper <SamuelNKasper@web.de>
2023-03-31 09:23:39 +0200

    test 6: jsonObj.end_devices_ids

--- server.ts
+++ server.ts
@@ -17,7 +17,8 @@ app.post('/uplink', async (req, res) => {
     //TODO: decide if device is already in db or is a new device
     // use dev_eui and add zeros at the end to fit 12byte and use it as id for mongodb
     let jsonObj = JSON.parse(JSON.stringify(req.body));
-    console.log(jsonObj);
+    console.log(jsonObj.end_device_ids);
+
     /*let dev_eui = jsonObj.data.end_device_ids.dev_eui;
 
     let data = {
