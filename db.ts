7f1c7f98f92b779cbaab45a75a97c87286fe7e12
SamuelKasper <SamuelNKasper@web.de>
2023-04-18 13:38:06 +0200

    test

--- db.ts
+++ db.ts
@@ -1,4 +1,4 @@
-import {Document, MongoClient, ObjectId} from "mongodb";
+import {MongoClient, ObjectId} from "mongodb";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -50,10 +50,9 @@ export async function db_updateDBbyUplink(_devEUI: string, data: {}, base_data:{
 
         // No db entrie was found
         if(result.length == 0){
-            let obj = (JSON.stringify(data));
-            let res = await collection.insertOne({
-                obj
-                /*
+            let obj = JSON.parse(JSON.stringify(data));
+            let res = await collection.insertOne(obj
+                /*{
                 // Always there
                 time: <string> obj.time,
                 dev_eui: <string> obj.dev_eui,
@@ -90,17 +89,19 @@ export async function db_updateDBbyUplink(_devEUI: string, data: {}, base_data:{
                 hum_min: `${obj.hum_min}`,
                 hum_max: `${obj.hum_max}`,
                 description:`${obj.description}`,
-                watering_time:`${obj.watering_time}`*/
-            });
+                watering_time:`${obj.watering_time}`
+            }*/
+            );
                 console.log("Generated new db entrie with id: " +res.insertedId);
         }else{
             // if there is a db entry, get id from entrie and update
             let res_obj = JSON.parse(JSON.stringify(result));
-            let obj = (JSON.stringify(base_data));
+            let obj = JSON.parse(JSON.stringify(base_data));
 
-            let res = await collection.updateOne({"_id": new ObjectId(res_obj[0]._id)},{
-                obj
-                /*$set:{
+            let res = await collection.updateOne({"_id": new ObjectId(res_obj[0]._id)},
+                obj 
+                /*{
+                $set:{
                 gateway:`${obj.gateway}`,
                 time:`${obj.time}`,
                 dev_eui:`${obj.dev_eui}`,
@@ -110,8 +111,8 @@ export async function db_updateDBbyUplink(_devEUI: string, data: {}, base_data:{
                 air_humidity:`${obj.air_humidity}`,
                 soil_temperature:`${obj.soil_temperature}`,
                 soil_humidity:`${obj.soil_humidity}`,
-                distance:`${obj.distance}`*/
-            });
+                distance:`${obj.distance}`
+            }*/);
             console.log("found: "+ res.matchedCount +" entrie.", "\nupdated id: " + res_obj[0]._id);
         }
     }catch(e){
