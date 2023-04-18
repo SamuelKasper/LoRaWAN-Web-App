78c4d4c885b7d99ce0f6a31a83878cf577053f4d
SamuelKasper <SamuelNKasper@web.de>
2023-04-18 13:54:46 +0200

    insert_data -> $set: insert_date

--- build/db.js
+++ build/db.js
@@ -93,68 +93,16 @@ function db_updateDBbyUplink(_devEUI, data, base_data) {
             let result = yield collection.find({ "dev_eui": _devEUI }).toArray();
             // No db entrie was found
             if (result.length == 0) {
-                let obj = JSON.parse(JSON.stringify(data));
-                let res = yield collection.insertOne(obj
-                /*{
-                // Always there
-                time: <string> obj.time,
-                dev_eui: <string> obj.dev_eui,
-                name: <string> obj.name,
-                gateway: <string> obj.gateway,
-                rssi: <number> obj.rssi,
-                // Sensor data
-                air_temperature: <number> obj.air_temperature,
-                air_humidity: <number> obj.air_humidity,
-                soil_temperature: <string> obj.soil_temperature,
-                soil_humidity: <string> obj.soil_humidity,
-                distance: <number> obj.distance,
-                // Editable fields
-                max_distance: <number> obj.max_distance,
-                hum_min: <number> obj.hum_min,
-                hum_max: <number> obj.hum_max,
-                description: <string> obj.description,
-                watering_time: <string> obj.watering_time,*/
-                // Always there 
-                /*time:`${obj.time}`,
-                dev_eui:`${obj.dev_eui}`,
-                name:`${obj.name}`,
-                gateway:`${obj.gateway}`,
-                rssi:`${obj.rssi}`,
-                // Sensor data
-                air_temperature:`${obj.air_temperature}`,
-                air_humidity:`${obj.air_humidity}`,
-                soil_temperature:`${obj.soil_temperature}`,
-                soil_humidity:`${obj.soil_humidity}`,
-                distance:`${obj.distance}`,
-                // Editable fields
-                max_distance: `${obj.max_distance}`,
-                hum_min: `${obj.hum_min}`,
-                hum_max: `${obj.hum_max}`,
-                description:`${obj.description}`,
-                watering_time:`${obj.watering_time}`
-            }*/
-                );
+                let insert_data = JSON.parse(JSON.stringify(data));
+                let res = yield collection.insertOne(insert_data);
                 console.log("Generated new db entrie with id: " + res.insertedId);
             }
             else {
                 // if there is a db entry, get id from entrie and update
-                let res_obj = JSON.parse(JSON.stringify(result));
-                let obj = JSON.parse(JSON.stringify(base_data));
-                let res = yield collection.updateOne({ "_id": new mongodb_1.ObjectId(res_obj[0]._id) }, obj
-                /*{
-                $set:{
-                gateway:`${obj.gateway}`,
-                time:`${obj.time}`,
-                dev_eui:`${obj.dev_eui}`,
-                rssi:`${obj.rssi}`},
-                // Sensor data
-                air_temperature:`${obj.air_temperature}`,
-                air_humidity:`${obj.air_humidity}`,
-                soil_temperature:`${obj.soil_temperature}`,
-                soil_humidity:`${obj.soil_humidity}`,
-                distance:`${obj.distance}`
-            }*/ );
-                console.log("found: " + res.matchedCount + " entrie.", "\nupdated id: " + res_obj[0]._id);
+                let obj_id = JSON.parse(JSON.stringify(result));
+                let insert_data = JSON.parse(JSON.stringify(base_data));
+                let res = yield collection.updateOne({ "_id": new mongodb_1.ObjectId(obj_id[0]._id) }, { $set: insert_data });
+                console.log("found: " + res.matchedCount + " entrie.", "\nupdated id: " + obj_id[0]._id);
             }
         }
         catch (e) {
